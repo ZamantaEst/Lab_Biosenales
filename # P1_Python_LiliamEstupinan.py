7e7886937f2bbbccc3c96fa6951085db9db57cea
ZamantaEst <zamanta.estupinan@udea.edu.co>
2024-08-19 14:29:28 -0500

    Productos Changes to be committed: modified:   # P1_Python_LiliamEstupinan.py

--- # P1_Python_LiliamEstupinan.py
+++ # P1_Python_LiliamEstupinan.py
@@ -1,5 +1,10 @@
 # P1_Python_LiliamEstupinan
 import numpy as np
 import matplotlib as mp
-a=[3.1,1,0.5,-3.2,6]
+
+a=np.array([3.1,1,0.5,-3.2,6])
+b=np.array([1,3,2.2,5.1,1])
+
+ProdEscalar=a*b
+ProdPunto=np.dot(a,b)
 
